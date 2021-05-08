6ea34486942d526d46acacbf55c29b8010e765ac
Henrique F. Batista <hfbatista@hotmail.com>
2021-05-08 00:30:29 -0300

    transform Domain use case and models to public

--- Domain/CreateAccount.swift
+++ Domain/CreateAccount.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 
-protocol CreateAccount {
+public protocol CreateAccount {
 	func create(createAccountModel: CreateAccountModel, completion: @escaping (Result<AccountModel, Error>) ->  Void)
 }
 
-struct CreateAccountModel {
-	var name: String
-	var email: String
-	var password: String
-	var passwordConfirmation: String
+public struct CreateAccountModel {
+	public var name: String
+	public var email: String
+	public var password: String
+	public var passwordConfirmation: String
 }
