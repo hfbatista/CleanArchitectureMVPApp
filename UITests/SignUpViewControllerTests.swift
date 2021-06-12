1b160315358820fb97051e42842c69d14c3eb9e5
Henrique F. Batista <hfbatista@hotmail.com>
2021-06-12 01:07:02 -0300

    Add UI Layer

---
+++ UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by Henrique Batista on 12/06/21.
+//
+
+import XCTest
+@testable import UI
+
+class SignUpViewControllerTests: XCTestCase {
+
+    func test_() throws {
+    }
+
+}
