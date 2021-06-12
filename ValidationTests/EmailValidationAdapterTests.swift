c9f5e67ceae7ae77d81a0dc634a98cd753ec3d7f
Henrique F. Batista <hfbatista@hotmail.com>
2021-06-12 16:17:37 -0300

    add Validation Target

---
+++ ValidationTests/EmailValidationAdapterTests.swift
@@ -0,0 +1,13 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by Henrique Batista on 12/06/21.
+//
+
+import XCTest
+
+class EmailValidationAdapterTests: XCTestCase {
+
+    func test_() throws {}
+}
